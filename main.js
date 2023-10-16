481eb81f7b97b1061112d7fc7c0834792fc14c63
danargh <danargh86@gmail.com>
2023-10-16 07:03:41 +0700

    initial commmit

---
+++ main.js
@@ -0,0 +1,59 @@
+let label = document.querySelectorAll(".acc");
+
+label.forEach((e) => {
+   e.addEventListener("click", () => {
+      removeClass();
+      e.classList.toggle("active");
+   });
+});
+
+function removeClass() {
+   label.forEach((e) => {
+      e.classList.remove("active");
+   });
+}
+
+// Event Scroll
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+   var currentScrollPos = window.pageYOffset;
+   if (prevScrollpos > currentScrollPos) {
+      document.getElementById("navbar").style.top = "0";
+   } else {
+      document.getElementById("navbar").style.top = "-96px";
+   }
+   prevScrollpos = currentScrollPos;
+};
+
+window.addEventListener("scroll", function () {
+   if (window.scrollY >= 100) {
+      document.getElementById("navbar").style.backgroundColor = "#ffffff";
+      document.getElementById("navbar").style.boxShadow =
+         "0 0 10px rgba(0,0,0,0.1)";
+   } else {
+      document.getElementById("navbar").style.backgroundColor = "#ffffff";
+      document.getElementById("navbar").style.boxShadow = "none";
+   }
+});
+
+// Swiper
+var swiper = new Swiper(".mySwiper", {
+   pagination: {
+      el: ".swiper-pagination",
+   },
+   navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+   },
+});
+
+var swiper = new Swiper(".swiper-alumni-story", {
+   slidesPerView: 3,
+   spaceBetween: 30,
+   centeredSlides: true,
+   loop: true,
+   pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+   },
+});
